daab3b88651c69fcc223a96ec163cefcc8ba5fd8
Priyanka Kumari <kumaripriyanka1832@gmail.com>
2023-10-02 23:11:23 +0530

    making files

--- .cph/algorithm/hello.cpp
+++ .cph/algorithm/hello.cpp
@@ -1,8 +1,9 @@
+//simple program
 #include <iostream>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout<<"hello world 2.0";
+    cout<<"helloworld"<<endl;
     return 0;
 }
--- main.cpp
+++
@@ -1,9 +0,0 @@
- #include <iostream>
- using namespace std;
-
- int main(int argc, char const *argv[])
- {
-    cout<<"my name  is priyanka"<<endl;
-    return 0;
- }
- 
